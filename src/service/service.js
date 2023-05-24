e61b793afaa4c98f55a37a5794c19248a825cfc3
Surajit Maity <surajitmaity12345@gmail.com>
2023-05-24 16:38:38 +0530

    Updated api url

--- src/service/service.js
+++ src/service/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 async function getCurrencyRate(fromCurrency, toCurrency) {
-  const currencyRate = await axios.get(`https://currency-converter-server.onrender.com/api?fromCurrency=${fromCurrency}&toCurrency=${toCurrency}`);
+  const currencyRate = await axios.get(`https://currency-converter-server.vercel.app/api?fromCurrency=${fromCurrency}&toCurrency=${toCurrency}`);
   return currencyRate.data;
 }
 
